b8012653c0a17dc2bc2ac41655b7852ec9709c6a
Pooja Burande <42965036+PoojaRBurande@users.noreply.github.com>
2019-04-09 21:34:30 +0530

    table data populate using td tr

--- src/main/java/com/savings/controllers/PC.java
+++ src/main/java/com/savings/controllers/PC.java
@@ -39,3 +39,93 @@ public class PC {
 		}
 	}
 }
+
+/*<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$("#getDetails").click(function(){
+		alert("clicked");
+		var data="pooja";
+		$.ajax({
+			type: "POST",
+			url: "userDetail",
+			data: JSON.stringify(data),
+			success : function(UserBean){
+				alert(UserBean.address);
+				var htmlRow ="<tr><td>"+UserBean.username+"</td></tr>";
+				$("#userTable").append(htmlRow);
+			}
+		})
+	});
+
+	
+});
+</script>
+</head>
+<body>
+<input type="text" id="userId" name="userId">
+<button id="getDetails" name="getDetails">get</button>
+
+<table class="table table-striped table-fixed table-hover table-bordered" id="userTable" name="userTable">
+<thead>
+<tr>
+<th>UserName</th>
+<th> Mobile Number</th>
+<th> Email Address</th>
+<th> Date of Birth</th>
+<th> Address</th>
+</tr>
+</thead>
+</table>
+</body>
+</html>
+
+	controller part-----------------
+@RequestMapping(value = "/userDetail", method = RequestMethod.POST)
+	public @ResponseBody UserBean getUserDetails(@RequestBody String userid) {
+		
+		String userId = "pooja";
+		UserBean user= userHelper.getUser(userId);
+		return user;
+	} 
+	Data base call-------------------
+	@SuppressWarnings("rawtypes")
+		UserBean customer = (UserBean)getJdbcTemplate().queryForObject(
+				query, new Object[] { userId }, 
+		new BeanPropertyRowMapper(UserBean.class));
+	
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
